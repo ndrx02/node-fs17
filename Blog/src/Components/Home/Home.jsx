0a8bf50c869faeec9f0afd9ca176ba418588f25a
St3fan097 <Ste97.esse@gmail.com>
2023-12-12 15:07:40 +0100

    blog_gruppoB

--- Blog/src/Components/Home/Home.jsx
+++ Blog/src/Components/Home/Home.jsx
@@ -1,9 +1,13 @@
 import { PostPreview } from "../Post/PostPreview/PostPreview"
 import { Link } from "react-router-dom"
+import { Button } from "../Shared_button/Button"
 
 export const Home = () => {
+
+  const linkButton = ["Facebook", "Linkedin", "Twitter", "Github", "Instagram"]
   return(
     <div>
+      {linkButton.map((el, index)=> <Button name= {el} />)}
       <PostPreview/>
       <Link to="/post">
         {" "}
---
+++ Blog/src/Components/Shared_button/Button.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export function Button({ name }){
+const [] = useState("")
+
+const linkButton = []
+    return(
+        <>
+        <button>
+            <a>{name}</a>
+        </button>
+        </>
+    )
+}
