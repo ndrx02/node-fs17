700d9328d190fa23aab06b69a25b77a29bbd5092
Stef533 <137123241+Stef533@users.noreply.github.com>
2023-12-18 11:47:13 +0100

    craete css profile user

--- Blog/src/Components/ProfileUser/ProfileUser.jsx
+++ Blog/src/Components/ProfileUser/ProfileUser.jsx
@@ -1,9 +1,11 @@
+import "./ProfileUser.css"
+
 export function ProfileUser({nome, professione, avatarLink}){
   return(
-    <div>
-      <img src={avatarLink} alt="avatar"/>
+    <div className="user-card">
+      <img src={avatarLink} alt="avatar" className="avatar"/>
       <h1>{nome}</h1>
-      <p>{professione}</p>
+      <p className="job">{professione}</p>
     </div>
   )
 }
