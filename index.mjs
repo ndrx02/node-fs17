316194e18d106675d56c3c20af3bced4b9184a07
Eno-Mario <pisellienomario@gmail.com>
2023-12-21 17:04:08 +0100

    Feature: created backend

---
+++ index.mjs
@@ -0,0 +1,32 @@
+import express from "express";
+
+const postData = [];
+
+let idPost = 1;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+  res.json(postData);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json(postData.filter((el) => el.id === id));
+});
+
+app.post("/posts", (req, res) => {
+  const data = { id: idPost, ...req.body };
+  idPost++;
+
+  postData.push(data);
+
+  res.status(201).json("Post Created");
+});
+
+app.listen(3000, () => {
+  console.log("App is running!");
+});
