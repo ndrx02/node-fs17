650ec22b8ae01d58bb4556555b413c2c04d99128
Mrcgllmbrd <m.giallombardo91@gmail.com>
2024-01-08 11:46:48 +0100

    added get method

--- Blog/src/Components/Post/PostPreview/PostPreview.jsx
+++ Blog/src/Components/Post/PostPreview/PostPreview.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import "./postPreview.css"
+import axios from "axios"
 
 export const PostPreview = () => {
+  useState()
+
+  useEffect(()=>{
+    async function fetchData() {
+      const response = await axios.get('http://localhost:3000/posts')
+      console.log(response)
+   }
+    fetchData()
+  }, [])
+
   return (
     <div className="container-post">
       <img className="image-post-preview" src="https://avatars.githubusercontent.com/u/104137753?v=4" alt="image" />
