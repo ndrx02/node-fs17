612ef152805b75b6d28bd921ff8b620ac8421201
Adolfo-97 <adolfogomez@live.it>
2024-01-09 15:36:39 +0100

    modified array for post data

--- Blog/src/Components/Post/Post.jsx
+++ Blog/src/Components/Post/Post.jsx
@@ -1,28 +1,36 @@
 import { PostPreview } from "./PostPreview/PostPreview";
-import "./post.css"
+import "./post.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const Post = () => {
-  const [data, setData] = useState()
+  const [data, setData] = useState();
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get('http://localhost:3000/posts')
-      setData(
-        response.data[0]
-      )}
-    fetchData()
-  }, [])
-  console.log(data)
+      const response = await axios.get("http://localhost:3000/posts");
+
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
+  console.log(data);
 
   return (
     <div>
-      <PostPreview />
-      <div className="container-text">
-{/*         <h3></h3>
- */}        <p>{data && data.testo}</p>
-      </div>
+      {data &&
+        data.map((post) => {
+          return (
+            <div>
+              <PostPreview />
+              <div className="container-text">
+                {/*         <h3></h3>
+                 */}{" "}
+                <p>{post && post.testo}</p>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
